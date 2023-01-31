cf482671ebec5f9e7bf669a2eb0efdaa584b50a6
Mayuri2727 <mayuriasabe107@gmail.com>
2023-01-31 14:24:38 +0530

    SwapTwoNumbers

---
+++ SwapTwoNumbers.java
@@ -0,0 +1,18 @@
+package BasicCorePro;
+
+
+public class SwapTwoNumbers {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int t = 0;              // x and y are to swap
+
+        System.out.println("Two Numbers: "+a + "  " + b);
+        t = a;                   //swap
+        a = b;
+        b = t;
+        System.out.println("After Swap : "+a + "  " + b);
+        System.out.println( );
+    }
+}
+
