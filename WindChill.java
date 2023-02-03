4905ae5d28f0bc837126f917f445c0d916075e5a
Mayuri2727 <mayuriasabe107@gmail.com>
2023-02-03 16:16:32 +0530

    WindChill

---
+++ WindChill.java
@@ -0,0 +1,15 @@
+package BasicCorePro;
+
+public class WindChill {
+    public static void main(String[] args) {
+        double t = Double.parseDouble(args[0]);
+        double v = Double.parseDouble(args[1]);
+        if (Math.abs(t) > 50 || v > 120 || v < 3) {
+            System.out.println("Formula is not valid for the given input.");
+        } else {
+            double windChill = 35.74 + 0.6215 * t - 35.75 * Math.pow(v, 0.16) + 0.4275 * t * Math.pow(v, 0.16);
+            System.out.println("The wind chill is: " + windChill);
+        }
+
+    }
+}
